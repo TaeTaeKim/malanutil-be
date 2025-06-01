1210711473ac303ea378876bf9c2a511fcbb46ab
김태윤 <mygod456@kakao.com>
2025-06-01 10:25:35 +0900

    <fix> : Swagger Auth 포함이 안되는 버그 픽스

--- src/main/kotlin/taeyun/malanalter/config/SwaggerConfig.kt
+++ src/main/kotlin/taeyun/malanalter/config/SwaggerConfig.kt
@@ -1,30 +1,32 @@
 package taeyun.malanalter.config
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.security.SecurityScheme
-import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@SecurityScheme(
-    name = "Bearer Authentication",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    scheme = "bearer"
-)
 class SwaggerConfig {
     @Bean
     fun openAPI(): OpenAPI{
-        return OpenAPI().components(Components()).info(configurationInfo())
-    }
+        val securityScheme = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER)
+            .name("Authorization");
 
-    private fun configurationInfo(): Info {
-        return Info()
-            .title("MalanGG Alerter")
-            .description("매렌지지 알람 서버 API DOC")
-            .version("0.0.1")
+        // Security Requirement 정의
+        val securityRequirement = SecurityRequirement().addList("BearerAuth");
+
+        return OpenAPI()
+            .info(Info().title("Malangg Alerter API")
+                .description("메랜지지 알람 API")
+                .version("v1.0")
+            )
+            .addSecurityItem(securityRequirement)  // Security Requirement 추가
+            .schemaRequirement("BearerAuth", securityScheme);
     }
 }

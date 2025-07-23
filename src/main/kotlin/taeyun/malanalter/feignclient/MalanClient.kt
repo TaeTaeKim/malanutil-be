22a2a3cf184e789bb91e4972ea452c716adf55e5
김태윤 <mygod456@kakao.com>
2025-07-23 16:58:03 +0900

    hotfix : Malangg 요청에 UserAgent 헤더 적용.

--- src/main/kotlin/taeyun/malanalter/feignclient/MalanClient.kt
+++ src/main/kotlin/taeyun/malanalter/feignclient/MalanClient.kt
@@ -1,5 +1,6 @@
 package taeyun.malanalter.feignclient
 
+import feign.Headers
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.cloud.openfeign.SpringQueryMap
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,6 +12,7 @@ import taeyun.malanalter.alertitem.dto.MalanggBidRequest
 interface MalanClient {
 
     @GetMapping
+    @Headers("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     fun getItemBidList(
         @RequestParam("itemCode") itemCode: Int,
         @SpringQueryMap malanggBidRequest: MalanggBidRequest

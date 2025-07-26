5c77c285937c6272d8489ca37170645f413772f7
김태윤 <mygod456@kakao.com>
2025-07-26 10:57:11 +0900

    hotfix: 등록 아이템 반환시에 UserId가 반환되지 않도록 변경

--- src/main/kotlin/taeyun/malanalter/alertitem/dto/RegisteredItem.kt
+++ src/main/kotlin/taeyun/malanalter/alertitem/dto/RegisteredItem.kt
@@ -9,8 +9,7 @@ data class RegisteredItem (
     val itemOptions: ItemCondition,
     val itemName: String,
     val tradeType: TradeType,
-    val isAlarm: Boolean,
-    val userId : Long
+    val isAlarm: Boolean
 ){
     constructor(alertItemEntity: AlertItemEntity) :this(
         id = alertItemEntity.id.value,
@@ -18,7 +17,6 @@ data class RegisteredItem (
         itemOptions= alertItemEntity.itemCondition,
         tradeType = alertItemEntity.tradeType,
         itemName = AlertItemRepository.itemNameMap[alertItemEntity.itemId] ?: "이름 없음",
-        isAlarm = alertItemEntity.isAlarm,
-        userId = alertItemEntity.userId
+        isAlarm = alertItemEntity.isAlarm
     )
 }

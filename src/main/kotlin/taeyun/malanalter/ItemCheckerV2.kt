b7ab877b8c8f6ede977554153ff341e96f6c78da
김태윤 <mygod456@kakao.com>
2025-07-30 18:01:53 +0900

    hotfix: 비활성 유저의 Item Check Skip Log level을 debug로 조정

--- src/main/kotlin/taeyun/malanalter/ItemCheckerV2.kt
+++ src/main/kotlin/taeyun/malanalter/ItemCheckerV2.kt
@@ -60,7 +60,7 @@ class ItemCheckerV2(
                     logger.debug { "[User Coroutine] Start processing for User:$userId on thread: ${Thread.currentThread().name}" }
                     val userEntity = allUserEntityMap[userId] ?: return@launch
                     if (userEntity.disabled) {
-                        logger.warn { "[User Coroutine] User:$userId is disabled, skipping." }
+                        logger.debug { "[User Coroutine] User:$userId is disabled, skipping." }
                         return@launch
                     }
                     if (userEntity.isAlarmOff() || userEntity.isNotAlarmTime()) return@launch
